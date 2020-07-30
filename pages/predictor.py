c50ee1f8698fe1b5e72cacc1eed9c6ba678bfb28
Andy Tran <xpandalord@gmail.com>
2020-07-30 16:49:10 -0400

    updates

--- pages/predictor.py
+++ pages/predictor.py
@@ -5,7 +5,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from joblib import load
-pipeline = load('assets/pipeline.joblib')
+pipeline = load('notebooks/league-pipeline.joblib')
 
 # Imports from this application
 from app import app
